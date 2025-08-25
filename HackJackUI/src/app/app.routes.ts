da1a5aea57d5bc2e2596bc36c8a443090eaec966
rachel-duffy <rachelklesius@gmail.com>
2025-08-25 00:05:15 -0400

    fixing image, making basic homepage

--- HackJackUI/src/app/app.routes.ts
+++ HackJackUI/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Homepage} from './homepage/homepage';
+import {Play} from './play/play';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Homepage },
+  { path: 'play', component: Play}
+];
